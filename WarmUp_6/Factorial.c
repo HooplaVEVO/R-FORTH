61b02590b21fb210e67396a0002a8254810c8fcd
Myles Kaufman <myleskaufman.mk@gmail.com>
2024-01-28 22:50:15 +0000

    Added WarmUp_6

---
+++ WarmUp_6/Factorial.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(int argc,char** argv) {
+    int number = 5;
+    int result = factorial(number);
+    printf("Result: %d",result);
+    return 0;
+}
+
+int factorial(int n) {
+    if (n == 0){
+        return 1;
+    }
+    return n*factorial(n-1);
+}gi
